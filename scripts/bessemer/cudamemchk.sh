9dd6d7e4ab3fec309fa48554b91d26e9461d7e43
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2024-02-21 15:40:39 +0000

    Scripts updated for slurm and this assignment.

--- scripts/bessemer/cudamemchk.sh
+++ scripts/bessemer/cudamemchk.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-#$ -l gpu=1
-#$ -P rse
-#$ -q rse.q
-# Request 1 gigabytes of real memory (mem) per core
-#$ -l rmem=1G
-# Request 1 core in an OpenMP environment
-#$ -pe openmp 1
-# Name the job
-#$ -N com4521_cudamemchk
 # Request 10 minutes of time (This should be more than enough for your assignment)
-#$ -l h_rt=00:10:00
+#SBATCH --time=00:10:00
+# Name the job
+#SBATCH --job-name=com4521_cudamemchk
+# Name of file to which standard output will be redirected
+#SBATCH --output="com4521_profile_output.out"
+# Name of file to which the standard error stream will be redirected
+#SBATCH --error="com4521_profile_error.err"
 # To enable email notification, update the email address
-#$ -M me@somedomain.com
+#SBATCH --mail-user=me@somedomain.com
 # Email notifications if the job begins/ends/aborts (remove the characters as desired)
-#$ -m bea
+# Other options are ALL or NONE
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Request a DCS v100 test queue (max time limit 30 mins, but higher priority)
+#SBATCH --partition=dcs-gpu-test
+#SBATCH --account=dcs-res
+#SBATCH --gres=gpu:1
+
+# Request 4 cores
+#SBATCH --cpus-per-task=4
+# Request 1 gigabytes of real memory (mem) per core
+#SBATCH --mem-per-cpu=1G
 
 # Load Modules
-module load libs/CUDA/11.6.0/binary
-module load dev/gcc/8.2
+module load CUDAcore/11.1.1
+module load gcccuda/2019b
 
 # Run the compiled program
+# This example runs the Standard Deviation algorithm in CUDA mode, passing seed 12 and length 100
 
-compute-sanitizer --print-limit 1 "bin/debug/Particles" CUDA 100 512x512 output_image.png --bench
+compute-sanitizer --print-limit 1 "./bin/release/assignment" CUDA SD 12 100
--- scripts/bessemer/run.sh
+++ scripts/bessemer/run.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-#$ -l gpu=1
-#$ -P rse
-#$ -q rse.q
-# Request 1 gigabytes of real memory (mem) per core
-#$ -l rmem=1G
-# Request 4 cores in an OpenMP environment (this also causes 4 GPUs to be requested!)
-#$ -pe openmp 4
-# Name the job
-#$ -N com4521_run
 # Request 10 minutes of time (This should be more than enough for your assignment)
-#$ -l h_rt=00:10:00
+#SBATCH --time=00:10:00
+# Name the job
+#SBATCH --job-name=com4521_run
+# Name of file to which standard output will be redirected
+#SBATCH --output="com4521_run_output.out"
+# Name of file to which the standard error stream will be redirected
+#SBATCH --error="com4521_run_error.err"
 # To enable email notification, update the email address
-#$ -M me@somedomain.com
+#SBATCH --mail-user=me@somedomain.com
 # Email notifications if the job begins/ends/aborts (remove the characters as desired)
-#$ -m bea
+# Other options are ALL or NONE
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Request a DCS v100 test queue (max time limit 30 mins, but higher priority)
+#SBATCH --partition=dcs-gpu-test
+#SBATCH --account=dcs-res
+#SBATCH --gres=gpu:1
+
+# Request 4 cores
+#SBATCH --cpus-per-task=4
+# Request 1 gigabytes of real memory (mem) per core
+#SBATCH --mem-per-cpu=1G
 
 # Load Modules
-module load libs/CUDA/11.6.0/binary
-module load dev/gcc/8.2
+module load CUDAcore/11.1.1
+module load gcccuda/2019b
 
 # Run the compiled program
+# This example runs the Standard Deviation algorithm in OPENMP mode, passing seed 12 and length 100
 
-./bin/release/Particles CUDA 100 512x512 output_image.png --bench
+./bin/release/assignment OPENMP SD 12 100
+
+# You could perform multiple runs in a single batch, you can output to the output log with "echo <message>"
